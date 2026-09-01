d50449ef8939cd86832e697de709fcefe45e975b
Anshul Siwach <anshulsiwach07@gmail.com>
2026-09-01 15:44:32 +0530

    link added

--- aniket_website/src/App.jsx
+++ aniket_website/src/App.jsx
@@ -427,6 +427,33 @@ function App() {
               </svg>
               <span>YouTube</span>
             </a>
+
+            <a
+              className="footer-social"
+              href="https://www.facebook.com/Astrologeraniketsharma"
+              target="_blank"
+              rel="noreferrer"
+              aria-label="Visit Facebook"
+            >
+              <svg viewBox="0 0 24 24" aria-hidden="true">
+                <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" fill="currentColor" />
+              </svg>
+              <span>Facebook</span>
+            </a>
+          </div>
+
+          <div className="footer-services-section">
+            <p className="services-provided-label">Services Provided In</p>
+            <div className="footer-flags" aria-label="Countries we serve">
+              <img src="https://flagcdn.com/w80/in.png" alt="India" className="flag-circle" title="India" />
+              <img src="https://flagcdn.com/w80/cn.png" alt="China" className="flag-circle" title="China" />
+              <img src="https://flagcdn.com/w80/us.png" alt="United States" className="flag-circle" title="United States" />
+              <img src="https://flagcdn.com/w80/de.png" alt="Germany" className="flag-circle" title="Germany" />
+              <img src="https://flagcdn.com/w80/gb.png" alt="United Kingdom" className="flag-circle" title="United Kingdom" />
+              <img src="https://flagcdn.com/w80/ru.png" alt="Russia" className="flag-circle" title="Russia" />
+              <img src="https://flagcdn.com/w80/au.png" alt="Australia" className="flag-circle" title="Australia" />
+              <img src="https://flagcdn.com/w80/ca.png" alt="Canada" className="flag-circle" title="Canada" />
+            </div>
           </div>
         </div>
         <p className="footer-copyright">&copy; {new Date().getFullYear()} Aniket Sharma. All rights reserved.</p>
